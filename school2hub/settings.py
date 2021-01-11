f1146a982cbd500c94176032b1ed141b75bd50d7
Intellect <odetechit@gmail.com>
2021-01-11 12:10:32 +0100

    final push to heroku

--- school2hub/settings.py
+++ school2hub/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY= os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'unilorin-glo.herokuapp.com']
 
